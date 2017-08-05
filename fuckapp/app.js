ca74b5cad1b0b0e1ebc8b7db8c07e24c99c74ff8
Alejandro <alevelara@gmail.com>
2017-08-05 20:47:01 +0200

    BUILD SINGLE API AND EMAIL SENDER [APP TEST]

--- fuckapp/app.js
+++ fuckapp/app.js
@@ -7,35 +7,42 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-
+var cons = require('consolidate');
+var methodOverride = require('method-override');
 var mongo_server = require('./server')
 
 // servidor, socket.io y mongo
-//var server = require('http').createServer(app);
-//var io = require('socket.io')(server);
-
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 
 
 // view engine setup
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-//app.use(logger('dev'));
-//app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({ extended: false }));
-//app.use(cookieParser());
-//app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'));
 
-//app.use('/', index);
-//app.use('/users', users);
-
+app.use(cookieParser());
+app.use(methodOverride('_method'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+//var routes = require('./routes');
+//routes(app);
+
+var index = require('./routes/index')
+index(app)
 var routes = require('./routes/users')
 routes(app);
+var login = require('./routes/login')
+login(app);
+
+
+app.use(express.static(path.join(__dirname, 'public/javascripts/')));
 
 app.use(function(req,res){ 
     res.status(404).send({url: req.originalUrl + ' not found'});
