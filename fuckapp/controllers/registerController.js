cd72adc26d14fc86a7d4c8c0c6509cd28bbe90e7
Alejandro <alevelara@gmail.com>
2017-09-17 20:54:34 +0200

    Send json

--- fuckapp/controllers/registerController.js
+++ fuckapp/controllers/registerController.js
@@ -13,15 +13,15 @@ exports.login = function(req, res) {
         console.log(password);
         if (err){
             console.log('Login fail: date: %d', Date.now);            
-            return res.send(err);     
+            return res.json({error: err});     
             
         }else if(user == null){
             console.log('Incorrect user or password: date: %d', Date.now);
-           return res.send('404');
+           return res.json({message:'404'});
         }
         else            
             console.log('Login succes: name: %s password: %s - date: %d', email, password, Date.now);
-           return res.send('success');
+           return res.json({message:'success'});
     });
 };
 
@@ -40,12 +40,12 @@ function check_username(username, callback){
     var user_login = check_username(req.body.email, function(user_login){        
         if(user_login){
         console.log(user_login.email + " already exists. ");     
-        res.send('error');   
+        res.json({message: user_login.email + " already exists. "});  
         }else{
             var login = new user(req.body);            
             login.save(function(err, user){
             if(err){
-                return res.send(err);
+               res.json({error: err}); 
             }else{
                 console.log("Login Succesful");                
                 res.json({message:"success"}); 
