31d783a8bd5401f0a82759cd213c16007190010c
Alejandro <alevelara@gmail.com>
2017-09-24 21:07:58 +0200

    Fixing bugs and crypting pw [FIXED]

--- fuckapp/controllers/registerController.js
+++ fuckapp/controllers/registerController.js
@@ -9,21 +9,21 @@ exports.login = function(req, res) {
 
     passport.authenticate('local', function(err, user, info){
         var token;
+        console.log(user);
          if (err){
             console.log('Login fail: date: %d', Date.now.toString());
             res.status(404).json({error: err})
             return;
          }
-        if(user == null){
-            res.status(401).json({status:'error', error_message:'Incorrect user or password'});
-            console.log('Incorrect user or password: date: %d', Date.now.toString());
-            console.log(info);
+        if(!user){
+            res.status(401).json(info);
+            console.log('Incorrect user or password: date: %d', Date.now.toString());           
            return;
         }
         else{
             token = user.generateJwt();
             res.status(200);
-            res.json({status:'success', session_info:{"token": token}});            
+            res.json({status:'success', session_info:{"token":token}});            
            // console.log('Login succes: name: %s password: %s - date: %d', email, password, Date.now.toString());
            return;
         }                   
@@ -47,7 +47,10 @@ function check_username(username, callback){
         console.log(user_login.email + " already exists. ");     
         res.json({status:"error", error_message: user_login.email + " already exists. "});
         }else{
-            var newUser = new user(req.body);
+            var newUser = new user();
+
+            newUser.name = req.body.name;
+            newUser.email = req.body.email;
 
             newUser.setPassword(req.body.password);
             newUser.save(function(err, user){
@@ -59,7 +62,7 @@ function check_username(username, callback){
                 res.status(200);
                 console.log("Login Succesful"); 
                 console.log(token);                
-                res.json({"token": token, "message":"User save succesfully"});
+                res.json({"token": token, message:"User save succesfully"});
                 // After success login, we'll send a email verification          
                 mailCtrl.sendEmail(user.email);
             }  
