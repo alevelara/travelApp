2c1c45e352c3c3bec4d8e20ab2c8b75a4356136b
Alejandro <alevelara@gmail.com>
2017-11-15 20:14:23 +0100

    tests

--- fuckapp/tests/test.js
+++ fuckapp/tests/test.js
@@ -3,11 +3,25 @@ var serverUrl = require('../config/default.json');
 var shoud = require('chai').should(),
     expect = require('chai').expect(),
     supertest = require('supertest'),    
-    api = supertest(serverUrl.defaultUrl.DBHost);
+    api = supertest("http://localhost:8080");
 
 
-describe('get all users', function(done){
-    api.get('/users')
+describe('Users', function(){
+    it('should return all users', function(done){
+        api.get('/users')
     .set('Accept', 'application/json')
-    .expect(200, done);
+    .expect(200)
+    .end(function(err, res){
+        expect(res.body).to.have.property("name");
+        expect(res.body.name).to.not.be(null);
+        expect(res.body).to.have.property("email");
+        expect(res.body.email).to.not.be(null);
+        expect(res.body).to.have.property("hash");
+        expect(res.body.hash).to.not.be(null);
+        expect(res.body).to.have.property("salt");
+        expect(res.body.salt).to.not.be(null);        
+    });
+    done();
+    });
+
 });
