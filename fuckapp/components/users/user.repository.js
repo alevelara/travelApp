d4801868c96a42707fcd4d3e68d74398e339ba6e
eduardd <edusevilla90@gmail.com>
2018-02-06 19:55:50 +0100

    Fix merge conflicts

--- fuckapp/components/users/user.repository.js
+++ fuckapp/components/users/user.repository.js
@@ -49,17 +49,7 @@ exports.updateUserById = function(userId, user, callback){
         {
             full_name: user.full_name,
             username: user.username,
-<<<<<<< HEAD
-<<<<<<< 09e478d091337711a41c1f8c4e1f93031deb9159
             description: user.description
-=======
-            description: user.description,
-            photo_profile_id: user.photo_profile_id
->>>>>>> Add support for uploading/downloading photos.
-=======
-            description: user.description,
-            photo_profile_id: user.photo_profile_id
->>>>>>> d5aaa805f732ace498ec3a7bec3eef1904f3d503
         },
         {
             where: { id: userId },
@@ -75,15 +65,5 @@ exports.updateUserById = function(userId, user, callback){
         .catch(error=> {
             console.log(error)
             callback()}
-<<<<<<< HEAD
-<<<<<<< 09e478d091337711a41c1f8c4e1f93031deb9159
-            );
+    );
 }
-=======
-        );
-}
->>>>>>> Add support for uploading/downloading photos.
-=======
-        );
-}
->>>>>>> d5aaa805f732ace498ec3a7bec3eef1904f3d503
