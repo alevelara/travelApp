7d1de012c4cd13744442821bc6597dcda2d6ac6a
Edu Piñar <edusevilla90@gmail.com>
2018-02-16 12:35:53 +0100

    Fix queries.

--- fuckapp/components/userInterests/userInterest.repository.js
+++ fuckapp/components/userInterests/userInterest.repository.js
@@ -4,9 +4,9 @@ const TABLE_NAME = "userInterests";
 
 exports.getInterestsByUserId = function(userId){
     const querySQL =
-        `SELECT interest.* FROM interests interest ' +
-        'LEFT JOIN ${TABLE_NAME} ui ON interest.id = ui.interest_id ' +
-        'where ui.user_id = :user_id`;
+        `SELECT interest.* FROM interests interest ` +
+        `LEFT JOIN ${TABLE_NAME} ui ON interest.id = ui.interest_id ` +
+        `where ui.user_id = :user_id`;
 
     return Sequelize.query(querySQL,
         {
@@ -17,9 +17,9 @@ exports.getInterestsByUserId = function(userId){
 
 exports.getActiveInterestsByUserId = function(userId){
     const querySQL =
-        `SELECT interest.* FROM interests interest ' +
-        'LEFT JOIN ${TABLE_NAME} ui ON interest.id = ui.interest_id ' +
-        'where ui.user_id = :user_id AND ui.status = 1`;
+        `SELECT interest.* FROM interests interest ` +
+        `LEFT JOIN ${TABLE_NAME} ui ON interest.id = ui.interest_id ` +
+        `WHERE ui.user_id = :user_id AND ui.status = 1`;
 
     return Sequelize.query(querySQL, {
             replacements: { user_id: userId},
@@ -29,9 +29,9 @@ exports.getActiveInterestsByUserId = function(userId){
 
 exports.updateOrInsert = function(interest, userId) {
     const query =
-        `INSERT INTO ${TABLE_NAME} (interest_id, user_id, status) ' +
-        'VALUES(:interest_id, :user_id, :status) ' +
-        'ON DUPLICATE KEY UPDATE status=:status`;
+        `INSERT INTO ${TABLE_NAME} (interest_id, user_id, status) ` +
+        `VALUES(:interest_id, :user_id, :status) ` +
+        `ON DUPLICATE KEY UPDATE status=:status`;
 
     return Sequelize.query(query, {
         replacements: {
