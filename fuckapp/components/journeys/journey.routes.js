55c44fccd7d71d7a68d89acfe4b4a7ec696ea9b6
Alejandro <alevelara@gmail.com>
2018-07-12 19:19:48 +0200

    adding new changes

--- fuckapp/components/journeys/journey.routes.js
+++ fuckapp/components/journeys/journey.routes.js
@@ -8,4 +8,4 @@ module.exports = function(app) {
     app.get('/journey/all', secureRequest.validateSecureRequest, journeyController.getAllJourneys);
     app.get('/journey/:uuidJourney',secureRequest.validateSecureRequest, journeyController.getJourney);
     app.get('/journey/trip/:uuidTrip',secureRequest.validateSecureRequest, journeyController.getJourneysByUuidTrip);
-};
+};  
